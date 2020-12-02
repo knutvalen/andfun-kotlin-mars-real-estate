e508751eb10dc3e169e0b48ee0188ac9ae6c48f7
Knut Valen <knut.valen@icloud.com>
2020-12-02 09:33:24 +0100

    oijuytr

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,6 +17,25 @@
 
 package com.example.android.marsrealestate
 
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
 // TODO (06) Create the Binding Adapter, converting the imgUrl to a URI with the https scheme
 // TODO (07) Use Glide to load the imgUri into the imgView
 // TODO (11) Add the requestOptions for the placeholder and error into the Glide call
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    if (imgUrl != null) {
+        Glide.with(imgView.context).load(
+                imgUrl.toUri().buildUpon().scheme("https").build()
+        ).apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image)
+        ).into(imgView)
+    }
+}
